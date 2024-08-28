dc3846d3983a7893016e9e40e8df2360144b59b8
Monu Chouhan <chouhan.monu07@gmail.com>
2024-08-28 23:21:18 +0530

    init

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import { upperCase } from "lodash";
+
+console.log(
+  "%chello, world",
+  "color:white;background-color:indigo;border-radius:4px;"
+);
+console.log(upperCase("seven is a prime number."));
